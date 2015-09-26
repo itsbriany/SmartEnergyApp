a2d4bdc265e6994335e87b5446980fca78c93721
Brian Yip <itsbriany@gmail.com>
2015-09-26 18:57:40 -0300

    Figured out how to manipulate points on graph

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -15,17 +15,22 @@ angular.module('myApp.view2', ['ngRoute', 'chart.js'])
             $scope.dataset = response;
         });
 
-        $scope.updateChart = function() {
-            $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.updateChart = function(index) {
+            $scope.labels = [index, "February", "March", "April", "May", "June", "July"];
             $scope.series = ['Water Consumption', 'Electricity Consumption'];
             $scope.data = [
                 [65, 59, 80, 81, 56, 55, 40], // TODO This needs to be simulated!!
                 [28, 48, 40, 19, 86, 27, 90]
             ];
+
             $scope.onClick = function (points, evt) {
                 console.log(points, evt);
+                $scope.data = [
+                    [66, 66, 66, 66, 66, 66, 40], // TODO This needs to be simulated!!
+                    [11, 48, 11, 19, 11, 27, 90]
+                ];
             };
+
         };
 
-        $scope.updateChart();
 }]);
